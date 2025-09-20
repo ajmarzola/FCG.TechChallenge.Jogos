19b1b821c1e23b25f58b8f15adc5994b48376cc6
Anderson Marzola <ajmarzola@gmail.com>
2025-09-20 17:04:29 -0300

    Endpoints

--- src/FCG.TechChallenge.Jogos.Api/Endpoints/Jogos/CommandsEndpoints.cs
+++ src/FCG.TechChallenge.Jogos.Api/Endpoints/Jogos/CommandsEndpoints.cs
@@ -1,6 +1,8 @@
-﻿using MediatR;
-
-using FCG.TechChallenge.Jogos.Application.Commands.Jogos.CreateJogo;
+﻿using FCG.TechChallenge.Jogos.Application.Commands.Jogos.CreateJogo;
+using FCG.TechChallenge.Jogos.Application.Commands.Jogos.DeleteJogo;
+using FCG.TechChallenge.Jogos.Application.Commands.Jogos.UpdateJogo;
+using FCG.TechChallenge.Jogos.Application.Commands.Jogos.UpdateJogoPreco;
+using MediatR;
 
 namespace FCG.TechChallenge.Jogos.Api.Endpoints.Jogos
 {
@@ -14,8 +16,24 @@ namespace FCG.TechChallenge.Jogos.Api.Endpoints.Jogos
                 return Results.Created($"/jogos/{id}", new { id });
             });
 
-            // PUT /jogos/{id}/preco
-            // DELETE /jogos/{id}
+            app.MapPut("/jogos", async (UpdateJogoCommand cmd, ISender sender) =>
+            {
+                Guid id = await sender.Send(cmd);
+                return Results.NoContent();
+            });
+
+            app.MapPut("/jogos/{id:guid}/preco", async (Guid id, UpdateJogoPrecoCommand body, ISender sender) =>
+            {
+                await sender.Send(new UpdateJogoPrecoCommand(id, body.NovoPreco));
+                return Results.NoContent();
+            });
+
+            app.MapDelete("/jogos/{id:guid}", async (Guid id, ISender sender) =>
+            {
+                await sender.Send(new DeleteJogoCommand(id));
+                return Results.NoContent();
+            });
+
             return app;
         }
     }
