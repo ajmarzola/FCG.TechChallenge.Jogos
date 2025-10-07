d964a93527b1dfbcfb8d28b890320ed70e2deaec
Rafael Nicoletti <rafaelnicoletti@hotmail.com>
2025-10-06 23:44:03 -0300

    GetByIdAsync

--- src/FCG.TechChallenge.Jogos.Infrastructure/ReadModels/Elasticsearch/JogosReadRepositoryElastic.cs
+++ src/FCG.TechChallenge.Jogos.Infrastructure/ReadModels/Elasticsearch/JogosReadRepositoryElastic.cs
@@ -11,6 +11,14 @@ namespace FCG.TechChallenge.Jogos.Infrastructure.ReadModels.Elasticsearch
 
         public JogosReadRepositoryElastic(JogoSearchQueries q) => _q = q;
 
+        public async Task<JogoDto?> GetByIdAsync(Guid id, CancellationToken ct)
+        {
+            var res = await _q.SearchAsync(
+                termo: id.ToString(), page: 1, pageSize: 1,
+                categoria: null, precoMin: null, precoMax: null, sort: null, ct: ct);
+            return res.Items.FirstOrDefault();
+        }
+
         public async Task<Paged<JogoDto>> SearchAsync(string? termo, int page, int pageSize, CancellationToken ct)
         {
             var res = await _q.SearchAsync(
